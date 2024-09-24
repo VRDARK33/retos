fe24d872c590f3da7f944cc36c61e742b7135b09
VRDARK33 <brayan54sd@gmail.com>
2024-09-24 18:08:49 -0500

    retos

---
+++ reto00.py
@@ -0,0 +1,38 @@
+"""¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio."""
+ 
+ #https://www.python.org/
+ 
+ #comnetarios de una sola linea 
+ 
+"""comentarios de multiples lineas se hace usando las comillas triples """
+
+variable1 = 'hola mundo'
+
+entero = 2
+flotante = 3.4
+buleana = False
+buleana = True
+my_string = 'voy a ser el mejor ingeniero del mundo'
+
+print("hola python")
+
---
+++ reto01.py
@@ -0,0 +1,122 @@
+""" EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo."""
+
+#operadores aritmeticos
+
+suma = 2 + 2
+resta = 5 - 2
+multiplicacion = 2 * 2
+division = 9 / 2
+modulo = 2 % 2
+potencia = 2 ** 3
+division_entera = 5 // 2
+
+print(f'suma 2 + 2 = {suma}' )
+print(f'resta  5 - 2 = {resta}' )
+print(f'multiplicacion 2 * 2 = {multiplicacion}' )
+print(f'division 9 / 2 = {division}' )
+print(f'modulo 10 % 2 = {modulo}' )
+print(f'potencia 2 ** 3 = {potencia}' )
+print(f'division_entera 5 // 2 = {division_entera}' )
+
+
+#operadores de asignacion
+
+numero = 23
+print(numero) #asignacion
+numero += 1
+print(numero) # suma y asignacion
+numero -= 2
+print(numero) # resta y asignacion
+numero *= 4
+print(numero) # multiplicacion y asignacion
+numero /= 2
+print(numero) # division y asignacion
+numero %= 5
+print(numero) # modulo y asignacion
+numero **= 2
+print(numero) # exponenciacion y asignacion
+numero //= 3
+print(numero) # division entera y asignacion
+
+#operadores de comparacion 
+
+#igual a 
+print(f" 10 igual que 4  {10 == 4 }")
+#diferente de 
+print(f"10 diferente de 3  {10 != 3}")
+#mayor que
+print (f"10 es mayor que 3 {10 > 3}")
+#menor que 
+print(f"10 es menor que 3 {10 < 3}")
+#mayor o igual que
+print (f"10 es mayor o igual que 9 {10 >= 9}")
+#menor o igual que 
+print(f"10 es menor o igual que 10 {10 <= 10}")
+
+#operadores logicos
+
+#operado AND   
+print(f"5 > 3 and 3==3 {5>3 and 3==3}")
+
+#operado OR 
+print(f"5 > 6 or 2 < 4 { 5 > 6 or 2 < 4 }")
+
+#operador NOT 
+print(f"not 10 + 3 == 14 {not 10 + 3 == 14}")
+
+#operadores de identidad
+
+#operador is 
+
+
+numeroNuevo = numero
+
+print(f"b  apunta al mismo objetos en la memoria que a {numero is numeroNuevo}")
+
+
+#operador is not
+
+d = [1,2,3]
+c = d
+
+print(f"b  apunta al mismo objetos en la memoria que a {c is not d}")
+
+#operadores de pertenencia 
+
+#operador in
+
+a = [1,2,3]
+b = 2 
+
+print(f'b pertenece a a {b in a}')
+
+#operador not in
+
+a = [1,2,3]
+b = 4
+print(f'b no pertenece a a {b not in a}')
+
+
+#operadores bit a bit 
+
+#operador and 
+
+a = 5 # 0101
+b = 9 # 1001
+      # 0001
+
+print(a&b)
